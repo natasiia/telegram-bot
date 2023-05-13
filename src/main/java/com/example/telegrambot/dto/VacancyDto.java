40a6a9230e222ec415d2b691dddcc0efc5383905
Anastasiia Skrypnykova <46396153+natasiia@users.noreply.github.com>
2023-05-13 16:09:40 +0300

    Implemented reading from the file

--- src/main/java/com/example/telegrambot/dto/VacancyDto.java
+++ src/main/java/com/example/telegrambot/dto/VacancyDto.java
@@ -1,9 +1,16 @@
 package com.example.telegrambot.dto;
+
+import com.opencsv.bean.CsvBindByName;
+
 // dto = data transfer object
 public class VacancyDto {
+
     //main parameters of the vacancy
+    @CsvBindByName(column = "Id")
     private String id;
+    @CsvBindByName(column = "Title")
     private String title;
+    @CsvBindByName(column = "Short description")
     private String shortDescription;
 
     public String getId() {
--- src/main/java/com/example/telegrambot/service/VacancyService.java
+++ src/main/java/com/example/telegrambot/service/VacancyService.java
@@ -2,6 +2,7 @@ package com.example.telegrambot.service;
 
 import com.example.telegrambot.dto.VacancyDto;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -12,28 +13,17 @@ import java.util.List;
 @Service
 public class VacancyService {
     // save vacancies from various inputs
+    @Autowired
+    private VacanciesReaderService vacanciesReaderService;
     // key in the hashmap => String id, value => the vacancy itself
     private final Map<String, VacancyDto> vacancies = new HashMap<>();
 
     @PostConstruct
     public void init() {
-        VacancyDto juniorMaDeveloper = new VacancyDto();
-        juniorMaDeveloper.setId("1");
-        juniorMaDeveloper.setTitle("Junior Dev at MA");
-        juniorMaDeveloper.setShortDescription("Java Core is required!");
-        vacancies.put("1", juniorMaDeveloper);
-
-        VacancyDto googleDev = new VacancyDto();
-        googleDev.setId("3");
-        googleDev.setTitle("Junior Dev at Google");
-        googleDev.setShortDescription("Welcome to Google!");
-        vacancies.put("3", googleDev);
-
-        VacancyDto middle = new VacancyDto();
-        middle.setId("2");
-        middle.setTitle("Middle Java Dev");
-        middle.setShortDescription("Join our awesome company!");
-        vacancies.put("2", middle);
+        List<VacancyDto> list = vacanciesReaderService.getVacanciesFromFile("vacancies.csv");
+        for (VacancyDto vacancy: list) {
+            vacancies.put(vacancy.getId(), vacancy);
+        }
     }
 
     public List<VacancyDto> getJuniorVacancies() {
